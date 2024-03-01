e47e0ab1a65acff40011dc35aa5ca08e1f5c7ae3
YgorLino <ygorlino11@gmail.com>
2024-03-01 14:31:21 -0300

    adicionando pasta paginas e home e setando o banner da home
    Signed-off-by: YgorLino <ygorlino11@gmail.com>

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
+  
+  @Input() src: string = ''
+  
+  @Input() alt: string = ''
+
+
+  constructor(){
+
+  }
+  
+  ngOnInit(): void {
+    
+  }
 
 }
